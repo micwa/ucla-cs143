ba05ed193ffbb437a71669affd5197f25cf3bf14
Michael Wang <micwa6@yahoo.com>
2016-04-15 17:22:07 -0700

    Add last query and fix formatting

--- project1a/queries.sql
+++ project1a/queries.sql
@@ -1,19 +1,32 @@
-/*SELECT CONCAT(first,' ',last)
+-- Names of all actors in "Die Another Day"
+SELECT CONCAT(first, ' ', last)
 FROM MovieActor
-INNER JOIN Actor on MovieActor.aid=Actor.id
-WHERE mid = (SELECT id FROM Movie WHERE title = 'Die Another Day');
-*/
+INNER JOIN Actor ON MovieActor.aid = Actor.id
+WHERE mid =
+    (SELECT id FROM Movie
+     WHERE title = 'Die Another Day');
 
-/*SELECT count(*) FROM (SELECT aid FROM MovieActor GROUP BY aid HAVING COUNT(mid) > 1) as t1;
-*/
 
-/*SELECT title
+-- Count of all actors who have acted in multiple movies
+SELECT COUNT(*) FROM
+    (SELECT aid FROM MovieActor
+     GROUP BY aid
+     HAVING COUNT(*) > 1) AS t1;
+
+-- Title of movies that sell more than 1000000 tickets
+SELECT title
 FROM Movie
-INNER JOIN Sales on Movie.id=Sales.mid
+INNER JOIN Sales ON Movie.id = Sales.mid
 WHERE ticketsSold > 1000000;
-*/
 
-SELECT genre, avg(ticketsSold)
+-- Rank each year by average IMDb score of movies in that year, in descending order
+SELECT year, AVG(imdb) AS IMDb FROM Movie
+INNER JOIN MovieRating ON Movie.id = MovieRating.mid
+GROUP BY year
+ORDER BY IMDb DESC;
+
+-- Average tickets sold by genre
+SELECT genre, AVG(ticketsSold)
 FROM MovieGenre
-INNER JOIN Sales ON MovieGenre.mid=Sales.mid
+INNER JOIN Sales ON MovieGenre.mid = Sales.mid
 GROUP BY genre;

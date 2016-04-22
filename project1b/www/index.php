4eeba95d8c69534ca25f1ee1fa64f2a92d755a36
Michael Wang <micwa6@yahoo.com>
2016-04-22 12:37:57 -0700

    Add index.php, contents.html. Placeholder for search.php.

---
+++ project1b/www/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>CS143 - Project 1B</title>
+</head>
+<frameset cols="25%,*">
+    <frame src="contents.html" />
+    <frame src="search.php" />
+</frameset>
+</html>
---
+++ project1b/www/search.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>CS143 - Project 1B - Search</title>
+</head>
+<body>
+    <p>Temporary body for search.php</p>
+</body>
+</html>

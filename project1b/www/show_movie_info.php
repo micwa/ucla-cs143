e22d16eafb4b6f1e53390477c9847d7d499a235b
Michael Wang <micwa6@yahoo.com>
2016-04-28 23:04:58 -0700

    Edit show_movie_info

--- project1b/www/show_movie_info.php
+++ project1b/www/show_movie_info.php
@@ -3,9 +3,6 @@
     <title>CS143 - Project 1B - Show movie info</title>
 </head>
 <body>
-    <p>Temporary body for show_movie_info.php</p>
-
-    <hr />
     <!-- Search for actors/movies -->
     Search for actors/movies
     <form action="./search.php" method="GET">
@@ -13,65 +10,66 @@
         <input type="text" name="keyword" />
         <input type="submit" value="Search" />
     </form>
-     <?php
-        if (!isset($_GET["keyword"]) || $_GET["keyword"] === "")
-            die("No query entered.");
+    <hr />
 
-        $db = mysql_connect("localhost", "cs143", "");
-        if (!$db)
-            die("Unable to connect to database: " . mysql_error());
+    <?php
+    if (!isset($_GET["mid"]) || $_GET["mid"] === "")
+        die("No movie entered.");
 
-        $db_selected = mysql_select_db("CS143", $db);
-        if (!$db_selected)
-            die("Unable to select database: " . mysql_error());
+    $db = mysql_connect("localhost", "cs143", "");
+    if (!$db)
+        die("Unable to connect to database: " . mysql_error());
 
-        $mid = (int) $_GET["mid"];
+    $db_selected = mysql_select_db("CS143", $db);
+    if (!$db_selected)
+        die("Unable to select database: " . mysql_error());
 
-        $query = "SELECT * FROM Movie WHERE id=" . $mid;
-        if (!$result = mysql_query($query))
-            die("Error executing query: " . mysql_error());
-        if (mysql_num_rows($result) != 1)
-            die("No actor/actress with mid=$mid found.");
+    $mid = (int) $_GET["mid"];
 
-        $row = mysql_fetch_assoc($result);
-        echo "<h3> $row[title]</h3>\n";
-        echo "Year: $row[sex]<br />\n";
-        echo "Rating: $row[rating]<br />\n";
-        echo "Company: $row[r]]company<br />\n";
-        if (is_null($row[dod]))
-            echo "Date of death: N/A<br />\n";
-        else
-            echo "Date of death: $row[dod]<br />\n";
+    $query = "SELECT * FROM Movie WHERE id=" . $mid;
+    if (!$result = mysql_query($query))
+        die("Error executing query: " . mysql_error());
+    if (mysql_num_rows($result) != 1)
+        die("No actor/actress with mid=$mid found.");
 
-        mysql_free_result($result);
+    $row = mysql_fetch_assoc($result);
+    echo "<h3> $row[title]</h3>\n";
+    echo "Year: $row[sex]<br />\n";
+    echo "Rating: $row[rating]<br />\n";
+    echo "Company: $row[r]]company<br />\n";
+    if (is_null($row[dod]))
+        echo "Date of death: N/A<br />\n";
+    else
+        echo "Date of death: $row[dod]<br />\n";
 
-        // MovieActor info
-        $query = "SELECT mid, role, title FROM MovieActor
-                  INNER JOIN Movie ON MovieActor.mid = Movie.id
-                  WHERE aid=" . $aid;
-        if (!$result = mysql_query($query))
-            die("Error executing query: " . mysql_error());
+    mysql_free_result($result);
 
-        echo "<h4>Has acted in:</h4>\n";
-        echo "<table border=1 cellspacing=1 cellpadding=2>\n";
+    // MovieActor info
+    $query = "SELECT mid, role, title FROM MovieActor
+              INNER JOIN Movie ON MovieActor.mid = Movie.id
+              WHERE aid=" . $aid;
+    if (!$result = mysql_query($query))
+        die("Error executing query: " . mysql_error());
+
+    echo "<h4>Has acted in:</h4>\n";
+    echo "<table border=1 cellspacing=1 cellpadding=2>\n";
+    echo "<tr align=center>";
+    echo "<td><b>Movie</b></td>";
+    echo "<td><b>Role</b></td>";
+    echo "</tr>\n";
+    while ($row = mysql_fetch_assoc($result)) {
         echo "<tr align=center>";
-        echo "<td><b>Movie</b></td>";
-        echo "<td><b>Role</b></td>";
+        $mid = $row["mid"];
+        $role = $row["role"];
+        $title = $row["title"];
+        echo "<td><a href=\"./show_movie_info.php?mid=$mid\">$title</a></td>";
+        echo "<td>$role</td>";
         echo "</tr>\n";
-        while ($row = mysql_fetch_assoc($result)) {
-            echo "<tr align=center>";
-            $mid = $row["mid"];
-            $role = $row["role"];
-            $title = $row["title"];
-            echo "<td><a href=\"./show_movie_info.php?mid=$mid\">$title</a></td>";
-            echo "<td>$role</td>";
-            echo "</tr>\n";
-        }    
-        echo "</table>\n";
+    }    
+    echo "</table>\n";
 
-        mysql_free_result($result);
-        mysql_close($db);
-
-        ?>
+    mysql_free_result($result);
+    mysql_close($db);
+    ?>
 </body>
 </html>

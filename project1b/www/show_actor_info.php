d327e7200392f27e6009d8894783b237d615b243
Michael Wang <micwa6@yahoo.com>
2016-04-28 20:57:12 -0700

    Add search to both show_actor/movie_info

--- project1b/www/show_actor_info.php
+++ project1b/www/show_actor_info.php
@@ -4,5 +4,14 @@
 </head>
 <body>
     <p>Temporary body for show_actor_info.php</p>
+
+    <hr />
+    <!-- Search for actors/movies -->
+    Search for actors/movies
+    <form action="./search.php" method="GET">
+        Search:
+        <input type="text" name="keyword" />
+        <input type="submit" value="Search" />
+    </form>
 </body>
 </html>

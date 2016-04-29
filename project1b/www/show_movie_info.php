6518ee79779dfd1d864c603dc7f590c585fd6e4e
Lauren Yeung <lyeung.ca@gmail.com>
2016-04-28 22:57:40 -0700

    moviechanges

--- project1b/www/show_movie_info.php
+++ project1b/www/show_movie_info.php
@@ -25,9 +25,52 @@
         if (!$db_selected)
             die("Unable to select database: " . mysql_error());
 
-        $sql = $_GET["keyword"];
-        if (!$result = mysql_query($sql))
+        $mid = (int) $_GET["mid"];
+
+        $query = "SELECT * FROM Movie WHERE id=" . $mid;
+        if (!$result = mysql_query($query))
             die("Error executing query: " . mysql_error());
+        if (mysql_num_rows($result) != 1)
+            die("No actor/actress with mid=$mid found.");
+
+        $row = mysql_fetch_assoc($result);
+        echo "<h3> $row[title]</h3>\n";
+        echo "Year: $row[sex]<br />\n";
+        echo "Rating: $row[rating]<br />\n";
+        echo "Company: $row[r]]company<br />\n";
+        if (is_null($row[dod]))
+            echo "Date of death: N/A<br />\n";
+        else
+            echo "Date of death: $row[dod]<br />\n";
+
+        mysql_free_result($result);
+
+        // MovieActor info
+        $query = "SELECT mid, role, title FROM MovieActor
+                  INNER JOIN Movie ON MovieActor.mid = Movie.id
+                  WHERE aid=" . $aid;
+        if (!$result = mysql_query($query))
+            die("Error executing query: " . mysql_error());
+
+        echo "<h4>Has acted in:</h4>\n";
+        echo "<table border=1 cellspacing=1 cellpadding=2>\n";
+        echo "<tr align=center>";
+        echo "<td><b>Movie</b></td>";
+        echo "<td><b>Role</b></td>";
+        echo "</tr>\n";
+        while ($row = mysql_fetch_assoc($result)) {
+            echo "<tr align=center>";
+            $mid = $row["mid"];
+            $role = $row["role"];
+            $title = $row["title"];
+            echo "<td><a href=\"./show_movie_info.php?mid=$mid\">$title</a></td>";
+            echo "<td>$role</td>";
+            echo "</tr>\n";
+        }    
+        echo "</table>\n";
+
+        mysql_free_result($result);
+        mysql_close($db);
 
         ?>
 </body>

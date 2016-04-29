8ba98c0f75ac54cb3a89cc461f4bb5157c0156f7
Lauren <lyeung96@ucla.edu>
2016-04-28 21:58:19 -0700

    nothing much

--- project1b/www/show_movie_info.php
+++ project1b/www/show_movie_info.php
@@ -13,5 +13,22 @@
         <input type="text" name="keyword" />
         <input type="submit" value="Search" />
     </form>
+     <?php
+        if (!isset($_GET["keyword"]) || $_GET["keyword"] === "")
+            die("No query entered.");
+
+        $db = mysql_connect("localhost", "cs143", "");
+        if (!$db)
+            die("Unable to connect to database: " . mysql_error());
+
+        $db_selected = mysql_select_db("CS143", $db);
+        if (!$db_selected)
+            die("Unable to select database: " . mysql_error());
+
+        $sql = $_GET["keyword"];
+        if (!$result = mysql_query($sql))
+            die("Error executing query: " . mysql_error());
+
+        ?>
 </body>
 </html>
